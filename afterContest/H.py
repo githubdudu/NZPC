4dd9fd200e8e98411967778428c2e34cf3d13f17
hdon694 <hdon694@aucklanduni.ac.nz>
2024-08-27 12:58:05 +1200

    Corrected H

--- afterContest/H.py
+++ afterContest/H.py
@@ -51,34 +51,55 @@ def H():
     free2 = -1
     ft1 = abs(team1.strong.score - team2.weak.score)
     ft2 = abs(team1.weak.score - team2.strong.score)
+
+    # Tag if there are half turns
     if team1.strong.weaker(team2.weak) and ft1 % 2 == 1:
         free1 = 1
-    else:
+    elif team2.weak.weaker(team1.strong) and ft1 % 2 == 1:
         free1 = 2
     if team2.strong.weaker(team1.weak) and ft2 % 2 == 1:
         free2 = 2
-    else:
+    elif team1.weak.weaker(team2.strong) and ft2 % 2 == 1:
         free2 = 1
 
-    ft1 = (ft1 + ft1 % 2) / 2
-    ft2 = (ft2 + ft2 % 2) / 2
+    ft1 = (ft1 + ft1 % 2) / 2 # round up
+    ft2 = (ft2 + ft2 % 2) / 2 # round up
 
+    # Balance the half turns if same team have half turns
     if free1 == free2 and free1 == 1:
         ft1 -= 1
     elif free1 == free2 and free1 == 2:
         ft2 -= 1
 
+
+    weaker = None
+    stronger = None
     # display
     if ft1 == 0:
         print(f"No free turns between {team2.weak.name} and {team1.strong.name}.")
-        
+    
     else:
-        print(f"{team2.weak.name} receives {int(ft1)} free turns from {team1.strong.name}.")
+        if team2.weak.weaker(team1.strong):
+            weaker = team2.weak
+            stronger = team1.strong
+        else:
+            weaker = team1.strong
+            stronger = team2.weak
+
+        print(f"{weaker.name} receives {int(ft1)} free {'turn' if int(ft1) == 1 else 'turns'} from {stronger.name}.")
 
     if ft2 == 0:
         print(f"No free turns between {team1.weak.name} and {team2.strong.name}.")
         
     else:
-        print(f"{team1.weak.name} receives {int(ft2)} free turns from {team2.strong.name}.")
+        
+        if team1.weak.weaker(team2.strong):
+            weaker = team1.weak
+            stronger = team2.strong
+        else:
+            weaker = team2.strong
+            stronger = team1.weak
+
+        print(f"{weaker.name} receives {int(ft2)} free {'turn' if int(ft2) == 1 else 'turns'} from {stronger.name}.")
 H()
 # 0min
